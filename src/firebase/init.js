36ce7522f2f8995fb56fb80a719cb0abfd065008
henriqueebraga <52458912+henriqueebraga@users.noreply.github.com>
2021-06-29 11:29:46 +0200

    Update init.js

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -3,14 +3,6 @@ import firestore from 'firebase/firestore'
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyAngLubFT1mP_aQg-lC4Ah5vSLqM4kYXDk",
-    authDomain: "drinks-project-f0da8.firebaseapp.com",
-    databaseURL: "https://drinks-project-f0da8.firebaseio.com",
-    projectId: "drinks-project-f0da8",
-    storageBucket: "drinks-project-f0da8.appspot.com",
-    messagingSenderId: "831797877134",
-    appId: "1:831797877134:web:88897171f89e79ff8c6cdf",
-    measurementId: "G-LN6E99SNGB"
   };
   // Initialize Firebase
   const firebaseApp = firebase.initializeApp(firebaseConfig);
